5a5e9d03d0ed6d77c6eb5e620d420a31f233bee1
Abhishek Mishra <cs.abhishekmo@gmail.com>
2024-02-25 20:39:35 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
 const localhost = "http://localhost:3000";
-const testBackend = "https://chess-game-bt8o.onrender.com/"
+const testBackend = "https://chess-game-bt8o.onrender.com/";
+const droplet = "167.71.224.142:80"
 
 const socket = io(testBackend);
 
